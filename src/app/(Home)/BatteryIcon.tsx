f7f1450a1bafa41b443e46b2214868e557862b7a
Serena Inzani <serenainzani@hotmail.com>
2024-09-01 14:33:26 +0100

    battery icon reflects actual device charge

--- src/app/(Home)/BatteryIcon.tsx
+++ src/app/(Home)/BatteryIcon.tsx
@@ -1,5 +1,39 @@
-import { IconBattery4 } from "@tabler/icons-react";
+import {
+    IconBattery1,
+    IconBattery2,
+    IconBattery3,
+    IconBattery4,
+} from "@tabler/icons-react";
+import React, { useEffect, useState } from "react";
 
 export default function BatteryIcon() {
-    return <IconBattery4 size={90} color={"white"} />;
+    const [batteryLevel, setBatteryLevel] = useState(0.6);
+
+    const batteryLevelForIcon = (batteryLevel) => {
+        if (batteryLevel > 0.75) {
+            return <IconBattery4 size={90} color={"white"} />;
+        } else if (batteryLevel > 0.5) {
+            return <IconBattery3 size={90} color={"white"} />;
+        } else if (batteryLevel > 0.25) {
+            return <IconBattery2 size={90} color={"white"} />;
+        } else {
+            return <IconBattery1 size={90} color={"white"} />;
+        }
+    };
+
+    useEffect(() => {
+        async function getBatteryLevel() {
+            if ("getBattery" in navigator) {
+                const battery = await navigator.getBattery();
+                setBatteryLevel(battery.level);
+
+                battery.addEventListener("levelchange", () => {
+                    setBatteryLevel(battery.level);
+                });
+            }
+        }
+        getBatteryLevel();
+    }, []);
+
+    return batteryLevelForIcon(batteryLevel);
 }
