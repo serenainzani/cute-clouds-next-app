cb2c6204043e578bb7fa65e299575fc1f82e0cbf
Serena Inzani <serenainzani@hotmail.com>
2024-09-27 17:50:31 +0100

    feat: charging battery icon

--- src/app/(Home)/BatteryIcon.tsx
+++ src/app/(Home)/BatteryIcon.tsx
@@ -3,14 +3,18 @@ import {
     IconBattery2,
     IconBattery3,
     IconBattery4,
+    IconBatteryCharging,
 } from "@tabler/icons-react";
 import React, { useEffect, useState } from "react";
 
 export default function BatteryIcon() {
     const [batteryLevel, setBatteryLevel] = useState(0.6);
+    const [batteryCharging, setBatteryCharging] = useState(false);
 
-    const batteryLevelForIcon = (batteryLevel) => {
-        if (batteryLevel > 0.75) {
+    const setIcon = (batteryLevel, batteryCharging) => {
+        if (batteryCharging) {
+            return <IconBatteryCharging size={90} color={"white"} />;
+        } else if (batteryLevel > 0.75) {
             return <IconBattery4 size={90} color={"white"} />;
         } else if (batteryLevel > 0.5) {
             return <IconBattery3 size={90} color={"white"} />;
@@ -22,18 +26,24 @@ export default function BatteryIcon() {
     };
 
     useEffect(() => {
-        async function getBatteryLevel() {
+        async function getBatteryDetails() {
             if ("getBattery" in navigator) {
                 const battery = await navigator.getBattery();
                 setBatteryLevel(battery.level);
+                setBatteryCharging(battery.charging);
+                console.log(battery.charging);
 
                 battery.addEventListener("levelchange", () => {
                     setBatteryLevel(battery.level);
                 });
+
+                battery.addEventListener("chargingchange", () => {
+                    setBatteryCharging(battery.charging);
+                });
             }
         }
-        getBatteryLevel();
+        getBatteryDetails();
     }, []);
 
-    return batteryLevelForIcon(batteryLevel);
+    return setIcon(batteryLevel, batteryCharging);
 }
